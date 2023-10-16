d16dff27eff23e354591c28856b351835318910e
zyc <2498430685@qq.com>
2023-10-17 00:31:02 +0800

    拦截器优化

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,34 +1,27 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
+import io.netty.util.internal.StringUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //获取session
-        HttpSession session = request.getSession();
-        //湖区session中的用户
-        Object user = session.getAttribute("user");
-        //判断用户是否存在
-        if(user==null){
-            //不存在，拦截
-            response.setStatus(401);
-            return false;
-        }
-        //存在，保存到threadLocal
-        UserHolder.saveUser((UserDTO) user);
-        //放行
-        return true;
-    }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+       if(UserHolder.getUser()==null){
+           response.setStatus(401);
+           return false;
+       }
+        //放行
+        return true;
     }
 }
---
+++ src/main/java/com/hmdp/utils/RefershTokenInterception.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.hmdp.dto.UserDTO;
+import io.netty.util.internal.StringUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class RefershTokenInterception implements HandlerInterceptor {
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RefershTokenInterception(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("authorization");
+        if (StringUtil.isNullOrEmpty(token)) {
+            return true;
+        }
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        //将查到的Hash转为USerDto
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key);
+        if(userMap.isEmpty()){
+            return true;
+        }
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        //存在，保存到threadLocal
+        UserHolder.saveUser(userDTO);
+        //刷新token有效期
+        stringRedisTemplate.expire(key,30, TimeUnit.MINUTES);
+        //放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
